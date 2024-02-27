e50615be18194390a6c2635bc535d9ffd120843f
Abdul wasay khan <156195187+abdulwasayk14@users.noreply.github.com>
2024-02-27 10:11:11 +0500

    Add files via upload

---
+++ Alien_colors_1.js
@@ -0,0 +1,12 @@
+"use strict";
+let alien_color_passing = 'green';
+if (alien_color_passing === 'green') {
+    console.log("Congratulations! You just earned 5 points.");
+}
+let alien_color_falling = 'red'; // Change to 'yellow' to pass the if test
+if (alien_color_falling === 'green') {
+    console.log("Congratulations! You just earned 5 points.");
+}
+else {
+    // No output intentionally for the failing version
+}
---
+++ Alien_colors_1.ts
@@ -0,0 +1,13 @@
+let alien_color_passing = 'green';
+
+if (alien_color_passing === 'green') {
+    console.log("Congratulations! You just earned 5 points.");
+}
+
+let alien_color_falling = 'red'; // Change to 'yellow' to pass the if test
+
+if (alien_color_falling === 'green') {
+    console.log("Congratulations! You just earned 5 points.");
+} else {
+    // No output intentionally for the failing version
+}
